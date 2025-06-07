9c04882cd97b3ffc5eb226a893be5eb8d223c091
Nin Tran <nin.tran52@gmail.com>
2025-06-07 07:55:39 +0700

    Feat - re-structure the code

--- adapter/postgres/task_repository.go
+++ adapter/postgres/task_repository.go
@@ -1,4 +1,4 @@
-package repository
+package postgres
 
 import (
 	"github.com/nintran52/my-tasks/internal/domain/model"
--- usecase/task_usecase.go
+++ usecase/task_usecase.go
@@ -1,4 +1,4 @@
-package service
+package usecase
 
 import (
 	"github.com/nintran52/my-tasks/internal/domain/model"
